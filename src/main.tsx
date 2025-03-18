45846e93f4b1b1d5db117b98c34d8049196a3149
SummonRandomDeveloper <jasontran1963@gmail.com>
2025-03-17 21:59:11 -0500

    test main script

--- src/main.tsx
+++ src/main.tsx
@@ -11,9 +11,9 @@
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./App.css";
+// import "./App.css";
 import App from "./App.tsx";
-import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
